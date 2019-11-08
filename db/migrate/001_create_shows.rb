f2ab22a40b30487a03e196c1ea12629c6c1e4584
Steven Laff <steve.laff@gmail.com>
2019-11-08 01:11:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,3 +1,10 @@
-class Show
-  
-end
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :day
+      t.string :network
+      t.integer :rating
+    end
+  end
+end 
